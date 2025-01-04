3cea992d2a1ccec8a4596b5f38dfef7d74d5c31a
Subin hong <84978165+sss654654@users.noreply.github.com>
2025-01-04 13:39:17 +0000

    01/04 three BFS problem, 텀 프로젝트는 실패실패

--- BFS, DFS/벽 부수고 이동하기.py
+++ BFS, DFS/벽 부수고 이동하기.py
@@ -1,54 +1,89 @@
-M, N = map(int, input().split())
 
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+'''
+벽 부수고 이동하기
 
-miro = [[0 for i in range(N)] for k in range(M)]
-visit = [[[0 for i in range(2)] for k in range(N)] for j in range(M)]
-# visit = [방문거리, 벽 부순세계(1) / 부수지 않은 세계(0)]
+문제
+N×M의 행렬로 표현되는 맵이 있다. 맵에서 0은 이동할 수 있는 곳을 나타내고, 1은 이동할 수 없는 벽이 있는 곳을 나타낸다. 당신은 (1, 1)에서 (N, M)의 위치까지 이동하려 하는데, 이때 최단 경로로 이동하려 한다. 최단경로는 맵에서 가장 적은 개수의 칸을 지나는 경로를 말하는데, 이때 시작하는 칸과 끝나는 칸도 포함해서 센다.
 
-# M = Y, N = X
+만약에 이동하는 도중에 한 개의 벽을 부수고 이동하는 것이 좀 더 경로가 짧아진다면, 벽을 한 개 까지 부수고 이동하여도 된다.
 
-for col in range(M):
-    Nstr = input()
-    rowindex = 0
-    for row in Nstr:
-        miro[col][rowindex] = int(row)
-        rowindex += 1
+한 칸에서 이동할 수 있는 칸은 상하좌우로 인접한 칸이다.
 
+맵이 주어졌을 때, 최단 경로를 구해 내는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 1,000), M(1 ≤ M ≤ 1,000)이 주어진다. 다음 N개의 줄에 M개의 숫자로 맵이 주어진다. (1, 1)과 (N, M)은 항상 0이라고 가정하자.
+
+출력
+첫째 줄에 최단 거리를 출력한다. 불가능할 때는 -1을 출력한다.
+'''
+import sys
+N, M = map(int, sys.stdin.readline().split())
+maps = []
+
+for i in range(N):
+    maps.append(list(map(int, sys.stdin.readline().strip())))
+visited = [[[0 for i in range(2)] for j in range(M)] for k in range(N)]
+
+# [벽을 부수지 않은 세계, 벽을 부순 세계](6,4)
+'''
+for i in range(N):
+    print(visited[i])
+[[0, 0], [0, 0], [0, 0], [0, 0]]
+[[0, 0], [0, 0], [0, 0], [0, 0]]
+[[0, 0], [0, 0], [0, 0], [0, 0]]
+[[0, 0], [0, 0], [0, 0], [0, 0]]
+[[0, 0], [0, 0], [0, 0], [0, 0]]
+[[0, 0], [0, 0], [0, 0], [0, 0]]
+'''
+
+from collections import deque
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
 
 def BFS():
-    queue = []
-    queue.append([0, 0, 0])
-    visit[0][0][0] = 1
-    while len(queue) != 0:  # queue가 빌 경우
-        pairQ = queue.pop(0)
-
-        if pairQ[0] == M-1 and pairQ[1] == N-1:
-            return visit[pairQ[0]][pairQ[1]][pairQ[2]]
-
+    q = deque()
+    q.append([0,0,0])
+    visited[0][0][0] = 1 # 벽을 부수지 않은 셰게에서부터 시작
+    while q:
+        x,y,world = q.popleft()
+        if x == N-1 and y == M-1:
+            return visited[x][y][world]
         for dir in range(4):
-            nx = dx[dir] + pairQ[0]
-            ny = dy[dir] + pairQ[1]
-
-            if nx < 0 or nx >= M or ny < 0 or ny >= N:
+            nx = x + dx[dir]
+            ny = y + dy[dir]
+            if nx<0 or nx>=N or ny<0 or ny>=M:
                 continue
-            # N = Y, M = X
-            # 벽을 부순 세계 pairQ[2] = 1
-            # 벽을 아직 부수지 않은 세계로 나누어야 함 pairQ[2] = 0
-            if visit[nx][ny][pairQ[2]] != 0:
-                # visit[0] 0이 아니면 방문하여 찍은 것
+            if visited[nx][ny][world] != 0 :
                 continue
-            if miro[nx][ny] == 0:
-                # 미로가 막혀있지 않을 경우
-                visit[nx][ny][pairQ[2]] = visit[pairQ[0]
-                                                ][pairQ[1]][pairQ[2]] + 1
-                queue.append([nx, ny, pairQ[2]])
-            if miro[nx][ny] == 1 and pairQ[2] == 0:
-                # 미로가 막혀있고 벽을 부순적이 없는 경우
-                visit[nx][ny][1] = visit[pairQ[0]][pairQ[1]][pairQ[2]] + 1
-                queue.append([nx, ny, 1])
+            if world == 0 and maps[nx][ny] == 1: # 벽을 부수지 않은 세계에서 벽을 만날 경우 세계 변환
+                # 벽을 부수지 않은 세계에서 벽을 부순 세계로 넘어감([world] -> [1])
+                visited[nx][ny][1] = visited[x][y][world] + 1
+                q.append([nx,ny,1])
+            elif maps[nx][ny] == 0: # 어떤 세계든 벽이 안막혀있으면 진행
+                visited[nx][ny][world] = visited[x][y][world] + 1
+                q.append([nx,ny,world])
     return -1
 
-
 print(BFS())
+
+'''
+예제 입력 1 
+6 4
+0100
+1110
+1000
+0000
+0111
+0000
+예제 출력 1 
+15
+예제 입력 2 
+4 4
+0111
+1111
+1111
+1110
+예제 출력 2 
+-1
+'''
--- BFS, DFS/텀 프로젝트.py
+++ BFS, DFS/텀 프로젝트.py
@@ -1,35 +1,141 @@
+'''
+문제
+이번 가을학기에 '문제 해결' 강의를 신청한 학생들은 텀 프로젝트를 수행해야 한다. 프로젝트 팀원 수에는 제한이 없다. 심지어 모든 학생들이 동일한 팀의 팀원인 경우와 같이 한 팀만 있을 수도 있다. 프로젝트 팀을 구성하기 위해, 모든 학생들은 프로젝트를 함께하고 싶은 학생을 선택해야 한다. (단, 단 한 명만 선택할 수 있다.) 혼자 하고 싶어하는 학생은 자기 자신을 선택하는 것도 가능하다.
+
+학생들이(s1, s2, ..., sr)이라 할 때, r=1이고 s1이 s1을 선택하는 경우나, s1이 s2를 선택하고, s2가 s3를 선택하고,..., sr-1이 sr을 선택하고, sr이 s1을 선택하는 경우에만 한 팀이 될 수 있다.
+
+예를 들어, 한 반에 7명의 학생이 있다고 하자. 학생들을 1번부터 7번으로 표현할 때, 선택의 결과는 다음과 같다.
+
+1	2	3	4	5	6	7
+3	1	3	7	3	4	6
+위의 결과를 통해 (3)과 (4, 7, 6)이 팀을 이룰 수 있다. 1, 2, 5는 어느 팀에도 속하지 않는다.
+
+주어진 선택의 결과를 보고 어느 프로젝트 팀에도 속하지 않는 학생들의 수를 계산하는 프로그램을 작성하라.
+
+입력
+첫째 줄에 테스트 케이스의 개수 T가 주어진다. 
+각 테스트 케이스의 첫 줄에는 학생의 수가 정수 n (2 ≤ n ≤ 100,000)으로 주어진다. 
+각 테스트 케이스의 둘째 줄에는 선택된 학생들의 번호가 주어진다. (모든 학생들은 1부터 n까지 번호가 부여된다.)
+
+출력
+각 테스트 케이스마다 한 줄에 출력하고, 각 줄에는 프로젝트 팀에 속하지 못한 학생들의 수를 나타내면 된다.
+'''
 import sys
-sys.setrecursionlimit(1000000)
-input = sys.stdin.readline
-T = int(input())
+from collections import deque
+dx=[0,0]
+dy=[1,-1]
 
-while T != 0:
-    if T < 0:
-        break
-    T -= 1
-    n = int(input())
-    selpart = [int(x) for x in input().split()]
-    selpart.insert(0, 0)
+T = int(sys.stdin.readline())
+for i in range(T):
+    n = int(sys.stdin.readline())
+    team = list(map(int, sys.stdin.readline().split()))
+    for i in range(len(team)):
+        team[i] -= 1
+    visited = [0 for i in range(n)]
+    def BFS(idx):
+        q = deque()
+        q.append([[idx],team[idx]])
+        while q:
+            idx, nx = q.popleft()
+            if visited[nx] == 1: # 이미 팀이 이루어져있음
+                continue
+            if nx not in idx: # idx=[0],nx=1 -> idx[0,1],nx=4 
+                idx.append(nx)
+                q.append([idx, team[nx]]) 
+            else: # 어떻게든 인덱스에 접근하는 친구들끼리는 팀이 이루어짐 -> idx[0,1,4],nx=1
+                # (3,6) -> (3,team[6]) -> (3,team[team[6]])
+                # (3,6) -> (3,5) -> (3,3)
+                ''' # 시간초과
+                start = idx.index(nx) # 1
+                for i in range(start,len(idx)):
+                    visited[idx[i]] = 1
+                '''
+                # 시간초과
+                while True:
+                    if nx in idx:
+                        visited[idx.pop()] = 1
+                    else:
+                        break
+                return 0
+        return -1
+    
+    for i in range(n):
+        if visited[i] == 0:
+            BFS(i)
+    count = 0
+    print(visited.count(0))
+'''
+7
+5
+2 5 4 5 2
+6
+1 3 4 3 2 6
+13
+2 4 5 2 4 1 8 9 10 11 9 10 10
+10
+2 5 7 1 6 8 8 3 5 10
+10
+2 7 10 5 3 3 9 10 6 3
+6
+2 1 1 2 6 3
 
-    totalpair = []
+// ans
+3
+2
+8
+6
+8
+4
 
-    def recur(me, partner):  # 1,3\
-        if me != selpart[partner]:  # 1 != selpart[3]
-            if (partner == selpart[partner]):
-                totalpair.append(partner)
-                return
-            recur(me, selpart[partner])
-        if me == selpart[partner]:
-            totalpair.append(partner)
-            return
-        return
-# [1,3] [2,1] [3,3] [4,7] [5,3] [6,4] [7,6]
-    for i in range(1, n+1):
-        recur(i, selpart[i])  # [3,7]
-    paircount = 0
-    for i in range(1, n+1):
-        for k in totalpair:
-            if i == k:
-                paircount += 1
-                break
-    print(n - paircount)
+예제 입력 1 
+2
+7
+3 1 3 7 3 4 6
+8
+1 2 3 4 5 6 7 8
+예제 출력 1 
+3
+0
+
+0123456
+2026235
+
+// 1) 앞서클 뒷서클
+2
+5
+2 3 1 1 1
+5
+5 5 4 5 3
+
+// ans
+2
+2
+
+// 2) 1서클, n서클
+2
+5
+2 3 4 5 1
+5
+1 2 3 4 5
+
+// ans
+0
+0
+
+// 3) low 케이스
+4
+2
+1 1
+2
+2 2
+2
+1 2
+2
+2 1
+
+// ans
+1
+1
+0
+0
+'''
